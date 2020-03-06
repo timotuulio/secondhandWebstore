47855b648dde67037ed9a91bc3c7100c22709335
Timo Tuulio <timotuulio@hotmail.com>
2020-03-06 15:06:32 +0200

    initial app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-// Nothing to see here yet
+//import api from 'asd.js'/*'/routes/api.js'*/;
+const api = require('asd.js');
+const express = require('express');
+
+const app = express();
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send(api);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
+});
