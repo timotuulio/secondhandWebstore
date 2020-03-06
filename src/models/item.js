f52bb669327e04f55d33201273aaa879b1ef4b5f
Tapio Nevalainen <tapio.nevalainen@tuni.fi>
2020-03-06 12:42:24 +0000

    Add models directory and first draft model for item.

---
+++ src/models/item.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const ItemSchema = new mongoose.Schema({
+  
+  ownerID: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    min: 1,
+    required: true,
+    validate: Number.isInteger
+  },
+  image: {
+      type:String
+  },
+  description: {
+      type:String
+  }
+});
+
+const Item = mongoose.model('Item', ItemSchema);
+
+module.exports = {
+  Item: Item
+}
