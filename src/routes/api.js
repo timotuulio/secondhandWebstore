2cc7a79b51e7cff1117b21f3647cdbdf300461b1
Timo Tuulio <timotuulio@hotmail.com>
2020-03-09 18:15:25 +0200

    Remove double api-name from url-routes

--- src/routes/api.js
+++ src/routes/api.js
@@ -6,36 +6,36 @@ const itemController = require('../controllers/itemController');
 
 
 
-app.get('/api', (req, res) => {res.send("This is api");});
+router.get('/api', (req, res) => {res.send("This is api");});
 
-app.get('/api/user', (req, res) => {res.send("GET: This is all users");});
-app.post('/api/user', (req, res) => {res.send("POST: This is all users");});
-app.put('/api/user', (req, res) => {res.send("PUT: This is all users");});
-app.delete('/api/user', (req, res) => {res.send("DELETE: This is all users");});
+router.get('/user', (req, res) => {res.send("GET: This is all users");});
+router.post('/user', (req, res) => {res.send("POST: This is all users");});
+router.put('/user', (req, res) => {res.send("PUT: This is all users");});
+router.delete('/user', (req, res) => {res.send("DELETE: This is all users");});
 
-app.get('/api/user/:id', (req, res) => {res.send("GET: This is single user");});
-app.post('/api/user/:id', (req, res) => {res.send("POST: This is single user");});
-app.put('/api/user/:id', (req, res) => {res.send("PUT: This is single user");});
-app.delete('/api/user/:id', (req, res) => {res.send("DELETE: This is single user");});
+router.get('/user/:id', (req, res) => {res.send("GET: This is single user");});
+router.post('/user/:id', (req, res) => {res.send("POST: This is single user");});
+router.put('/user/:id', (req, res) => {res.send("PUT: This is single user");});
+router.delete('/user/:id', (req, res) => {res.send("DELETE: This is single user");});
 
-app.get('/api/item', (req, res) => {res.send("GET: This is items");});
-app.post('/api/item', (req, res) => {res.send("POST: This is items");});
-app.put('/api/item', (req, res) => {res.send("PUT: This is items");});
-app.delete('/api/item', (req, res) => {res.send("DELETE: This is items");});
+router.get('/item', (req, res) => {res.send("GET: This is items");});
+router.post('/item', (req, res) => {res.send("POST: This is items");});
+router.put('/item', (req, res) => {res.send("PUT: This is items");});
+router.delete('/item', (req, res) => {res.send("DELETE: This is items");});
 
-app.get('/api/item/:id', (req, res) => {res.send("GET: This is single item");});
-app.post('/api/item/:id', (req, res) => {res.send("POST: This is single item");});
-app.put('/api/item/:id', (req, res) => {res.send("PUT: This is single item");});
-app.delete('/api/item/:id', (req, res) => {res.send("DELETE: This is single item");});
+router.get('/item/:id', (req, res) => {res.send("GET: This is single item");});
+router.post('/item/:id', (req, res) => {res.send("POST: This is single item");});
+router.put('/item/:id', (req, res) => {res.send("PUT: This is single item");});
+router.delete('/item/:id', (req, res) => {res.send("DELETE: This is single item");});
 
-app.get('/api/itemsofusers', (req, res) => {res.send("GET: This is list of users with items");});
-app.post('/api/itemsofusers', (req, res) => {res.send("POST: This is list of users with items");});
-app.put('/api/itemsofusers', (req, res) => {res.send("PUT: This is list of users with items");});
-app.delete('/api/itemsofusers', (req, res) => {res.send("DELETE: This is list of users with items");});
+router.get('/itemsofusers', (req, res) => {res.send("GET: This is list of users with items");});
+router.post('/itemsofusers', (req, res) => {res.send("POST: This is list of users with items");});
+router.put('/itemsofusers', (req, res) => {res.send("PUT: This is list of users with items");});
+router.delete('/itemsofusers', (req, res) => {res.send("DELETE: This is list of users with items");});
 
-app.get('/api/itemsofusers/:id', (req, res) =>  {res.send("GET: This is list of a certain users items");});
-app.post('/api/itemsofusers/:id', (req, res) =>  {res.send("POST: This is list of a certain users items");});
-app.put('/api/itemsofusers/:id', (req, res) =>  {res.send("PUT: This is list of a certain users items");});
-app.delete('/api/itemsofusers/:id', (req, res) =>  {res.send("DELETE: This is list of a certain users items");});
+router.get('/itemsofusers/:id', (req, res) =>  {res.send("GET: This is list of a certain users items");});
+router.post('/itemsofusers/:id', (req, res) =>  {res.send("POST: This is list of a certain users items");});
+router.put('/itemsofusers/:id', (req, res) =>  {res.send("PUT: This is list of a certain users items");});
+router.delete('/itemsofusers/:id', (req, res) =>  {res.send("DELETE: This is list of a certain users items");});
 
 module.exports = router;
