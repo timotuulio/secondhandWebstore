70a4ab53a68d1189e0e56db011b9cd6abde2cde1
Tapio Nevalainen <tapio.nevalainen@tuni.fi>
2020-03-09 18:53:37 +0000

    Initialize usercontroller.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,1 +1,13 @@
 // Controller for api when searching users
+const User = require('../models/userModel');
+const xssFilters = require('xss-filters');
+
+
+module.exports = {
+
+    addUser(req,res){
+
+       
+    }
+
+}
