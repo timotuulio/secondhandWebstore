b29f5108f38391b27b8d31d2fed75de2fc3e19bf
Tapio Nevalainen <tapio.nevalainen@tuni.fi>
2020-03-16 14:13:08 +0000

    Add some Header components, change some directories.

--- frontend/components/headerComponent.js
+++
@@ -1,1 +0,0 @@
-// React component for the header
---
+++ frontend/src/components/headerComponent.js
@@ -0,0 +1,17 @@
+// React component for the header
+import Login from './loginComponent';
+import React from 'react';
+
+const Header = ({ }) => (
+
+      <div>
+        <h2>This is HEADER</h2>
+        <Login />
+        
+      </div>
+      
+   
+  )
+
+
+export default Header;
