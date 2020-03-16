3b1f2a9d588d70f4d09d435219d4dcf189ac8cb3
Timo Tuulio <timotuulio@hotmail.com>
2020-03-16 12:06:25 +0200

    New public file and index-file and something

---
+++ public/jsfile.js
@@ -0,0 +1,11 @@
+alert("lol");
+// const store = require('../src/store/store');
+// const AllItems = require('../src/components/itemComponent');
+//
+// const ReactApp = () => (
+//   <Provider store={store}>
+//     <AllItems />
+//   </Provider>
+// );
+//
+// render(<ReactApp />, document.getElementById('root'));
