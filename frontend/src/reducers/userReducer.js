a1fdfb37cd632099f75927c2114b889e3f2107bf
Timo Tuulio <timotuulio@hotmail.com>
2020-03-17 19:23:09 +0200

    Correct a bug

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -1,7 +1,7 @@
 import {QUEST, USER, SHOPKEEPER, ADMIN} from '../stateNames.js'
 
 const initialState = {
-  page: QUEST
+  role: QUEST
 };
 
 function userReducer(state = initialState, action) {
@@ -9,16 +9,16 @@ function userReducer(state = initialState, action) {
   switch(action.type) {
     case 'QUEST':
       console.log("changed state to QUEST")
-      return { ...state, page: QUEST };
+      return { ...state, role: QUEST };
     case 'USER':
       console.log("changed state to USER")
-      return { ...state, page: USER };
+      return { ...state, role: USER };
     case 'SHOPKEEPER':
       console.log("changed state to SHOPKEEPER")
-      return { ...state, page: SHOPKEEPER };
+      return { ...state, role: SHOPKEEPER };
     case 'ADMIN':
       console.log("changed state to ADMIN")
-      return { ...state, page: ADMIN };
+      return { ...state, role: ADMIN };
     default:
       return state;
   }
