df979922dd6775112583669897322e63dc387086
Tapio Nevalainen <tapio.nevalainen@tuni.fi>
2020-03-20 20:29:04 +0000

    Add Modal window for add item for sale confirmation

--- frontend/src/components/sellComponent.js
+++ frontend/src/components/sellComponent.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import { Button, FormFeedback, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import store from '../store/store.js';
 
 
- // take over its submit event.
+
+ 
+ 
+
+const SellItem = (props,{login,loginAction,loggedOutAction,user,ownProfileAction,userAddNewItemAction,mainAction}) => {
+
+
+
+    const {
+        buttonLabel,
+        className
+      } = props;
+    
+      const [modal, setModal] = React.useState(false);
+      const toggle = () => setModal(!modal);
+    // take over its submit event.
  let submit = (e) => {
     e.preventDefault();
   
@@ -24,19 +40,15 @@ import store from '../store/store.js';
         method : "post",
         headers: {'Content-Type': 'application/json','Authorization': 'Bearer '+store.getState().loginReducer.token},
         body: body
-    }).then(response => response.text()).then(html=> console.log(html));
+    }).then(response => response.text()).then(html=> toggle());
   }
   
- 
-  
 
+   
 
-
-
-
-const SellItem = ({login,loginAction,loggedOutAction,user,ownProfileAction,userAddNewItemAction,mainAction}) => {
-    
-
+    const [title, setTitle] = React.useState("");
+    const [description, setDescription] = React.useState("");
+    const [price, setPrice] = React.useState(0);
     
     const [titleOK, setTitleOk] = React.useState(false);
     const [descOK, setDescOk] = React.useState(false);
@@ -45,6 +57,7 @@ const SellItem = ({login,loginAction,loggedOutAction,user,ownProfileAction,userA
     var btn = document.getElementById('confirm');
 
     function checkTitle(e){
+        setTitle(e.target.value);
         if(e.target.value.length == 0){
             setTitleOk(false);
             btn.style.visibility = 'hidden';
@@ -57,6 +70,7 @@ const SellItem = ({login,loginAction,loggedOutAction,user,ownProfileAction,userA
     }
 
     function checkDescription(e){
+        setDescription(e.target.value);
         if(e.target.value.length == 0){
             setDescOk(false)
             btn.style.visibility = 'hidden';
@@ -70,6 +84,7 @@ const SellItem = ({login,loginAction,loggedOutAction,user,ownProfileAction,userA
     }
 
     function checkPrice(e){
+        setPrice(e.target.value);
         if(e.target.value.length != 0 && !isNaN(e.target.value)){
             setPriceOk(true)
         
@@ -108,14 +123,31 @@ const SellItem = ({login,loginAction,loggedOutAction,user,ownProfileAction,userA
             <Input id="price" onChange = {checkPrice} name="price" id="price" valid={ priceOK === true } invalid={ priceOK === false } />
             <FormFeedback invalid>Give correct price</FormFeedback>
             </FormGroup>
-            <Button style={{visibility:'hidden'}} id="confirm" block>Confirm</Button>
+
+                <div>
+                <Button style={{visibility:'hidden'}} onClick={toggle} id="confirm" block>Confirm</Button>
+              
+                <Modal isOpen={modal} toggle={toggle} className={className}>
+                    <ModalHeader toggle={toggle}>Confirm sale</ModalHeader>
+                    <ModalBody>
+                    <div>{title}</div>
+                    <div>{description}</div>
+                    <div>{price}</div>
+                    </ModalBody>
+                    <ModalFooter>
+                    <Button color="primary" onClick={submit}>Confirm</Button>{' '}
+                    <Button color="secondary" onClick={toggle}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
+            
        
         </Form>
         </div>
       );
 }
 
-
+//<Button color="danger" onClick={toggle}>{buttonLabel}</Button>
 
 
 export default SellItem;
