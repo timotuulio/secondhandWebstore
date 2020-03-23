1afe5cfdbc1a317ef4aa3f17527d2b55374e0e7c
Timo Tuulio <timotuulio@hotmail.com>
2020-03-23 23:32:59 +0200

    Beautify code

--- frontend/src/components/allUsersComponent.js
+++ frontend/src/components/allUsersComponent.js
@@ -36,99 +36,98 @@ function AllUsers({loadState, loadedAction, token, loadingAction}) {
         }
     }
 
-  }
-  fetch('http://localhost:3001/api/user', obj).then(res=>res.json()).then(data => setData(data)).then(loadedAction);
+}
+fetch('http://localhost:3001/api/user', obj).then(res => res.json()).then(data => setData(data)).then(loadedAction);
 
+function deleteUser(e) {
+    fetch('http://localhost:3001/api/user/' + e.target.value, {
+        method: 'delete',
+        headers: {
+            'authorization': 'Bearer ' + token
+        }
+    }).then(res => res.json()).then(data => loadingAction(), alert3());
+}
 
+var usersToRender = [];
 
-
-  function deleteUser(e){
-    fetch('http://localhost:3001/api/user/'+e.target.value,
-    {
-      method: 'delete',
-      headers: {
-        'authorization': 'Bearer ' + token}}).then(res=>res.json()).then(data => loadingAction(),alert3());
-  }
-
-
-  var usersToRender = [];
-
-  usersToRender.push(<Alert key="1" isOpen={visible2} color="success">
-  <h5 className="alert-heading">Offer removed successfully!</h5>
+usersToRender.push(<Alert key="1" isOpen={visible2} color="success">
+    <h5 className="alert-heading">Offer removed successfully!</h5>
 </Alert>)
 
-  //console.log(user);
-  if (loadState==='LOADED') {
-      var singleRoleArray = [];
-      singleRoleArray = user.filter(usr => usr.role === ADMIN);
-      singleRoleArray.map(usr => pushToArray(usr));
+//console.log(user);
+if (loadState === 'LOADED') {
+    var singleRoleArray = [];
+    singleRoleArray = user.filter(usr => usr.role === ADMIN);
+    singleRoleArray.map(usr => pushToArray(usr));
 
-      singleRoleArray = user.filter(usr => usr.role === SHOPKEEPER);
-      singleRoleArray.map(usr => pushToArray(usr));
+    singleRoleArray = user.filter(usr => usr.role === SHOPKEEPER);
+    singleRoleArray.map(usr => pushToArray(usr));
 
-      singleRoleArray = user.filter(usr => usr.role === USER);
-      singleRoleArray.map(usr => pushToArray(usr));
+    singleRoleArray = user.filter(usr => usr.role === USER);
+    singleRoleArray.map(usr => pushToArray(usr));
 
-  }
-  else {
-      usersToRender.push(
-          <div key="2">
-              <p>Loading...</p>
-          </div>)
-  }
-  return usersToRender
+} else {
+    usersToRender.push(<div key="2">
+        <p>Loading...</p>
+    </div>)
+}
+return usersToRender
 // TODO: Button is not yet connected to the users profile
-  function pushToArray(usr) {
+function pushToArray(usr) {
 
-    usersToRender.push(
-      <div  key={usr['_id']} style={{
-        display: "flex",
-        margin: "15px",
-        justifyContent: "center",
+    usersToRender.push(<div key={usr['_id']} style={{
+            display: "flex",
+            margin: "15px",
+            justifyContent: "center",
             alignItems: "center",
-        width:"100%"
+            width: "100%"
         }}>
         <Card style={{
-          maxWidth:"500px",
-          minWidth:"30em",
-          marginBottom:"5px",
-          alignSelf:"center"
-        }}>
-          <CardHeader>{usr.name} </CardHeader>
-          <CardBody>
-            <CardText>Role: {usr.role}</CardText>
-            <CardText>{usr.email}</CardText>
-            {(() => {
-              if (usr.address!==undefined && usr.address!=='undefined') {
-                return <CardText>Address: {usr.address}</CardText>;
-              }
-            })()}
-            {(() => {
-              if (usr.phoneNumber!==undefined && usr.phoneNumber!=='undefined') {
-                console.log("--------" + usr.phoneNumber)
-                return <CardText>Phonenumber: {usr.phoneNumber}</CardText>;
-              }
-            })()}
-            <div>
-            <div><Button color="primary" >Got to profile</Button></div>
-            <br/>
-            <Button color="primary" onClick={deleteUser} value={usr['_id']}>Delete user</Button>
-            </div>
+                maxWidth: "500px",
+                minWidth: "30em",
+                marginBottom: "5px",
+                alignSelf: "center"
+            }}>
+            <CardHeader>{usr.name}
+            </CardHeader>
+            <CardBody>
+                <CardText>Role: {usr.role}</CardText>
+                <CardText>{usr.email}</CardText>
+                {
+                    (() => {
+                        if (usr.address !== undefined && usr.address !== 'undefined') {
+                            return <CardText>Address: {usr.address}</CardText>;
+                        }
+                    })()
+                }
+                {
+                    (() => {
+                        if (usr.phoneNumber !== undefined && usr.phoneNumber !== 'undefined') {
+                            console.log("--------" + usr.phoneNumber)
+                            return <CardText>Phonenumber: {usr.phoneNumber}</CardText>;
+                        }
+                    })()
+                }
+                <div>
+                    <div>
+                        <Button color="primary">Got to profile</Button>
+                    </div>
+                    <br/>
+                    <Button color="primary" onClick={deleteUser} value={usr['_id']}>Delete user</Button>
+                </div>
 
-
-          </CardBody>
+            </CardBody>
         </Card>
-      </div>
-    )
-  }
+    </div>)
+}
 }
 
 const mapStateToProps = (state) => ({loadState: state.loadReducer.loadState, role: state.loginReducer.role, token: state.loginReducer.token});
 
 const mapDispatchToProps = (dispatch) => ({
-    mainAction: () => dispatch(mainAction()),
-    loadedAction: () => dispatch(loadedAction()),
-    loadingAction: () => dispatch(loadingAction())
+mainAction: () => dispatch(mainAction()),
+loadedAction: () => dispatch(loadedAction()),
+loadingAction: () => dispatch(loadingAction())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(AllUsers);
